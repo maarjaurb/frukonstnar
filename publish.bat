d89e89bdc04875bbeb0555fa1a97e1926df5e29d
Maarja Urb <mail@m-urb.de>
2017-05-16 17:45:37 +0200

    $1

--- publish.bat
+++ publish.bat
@@ -1,1 +1,9 @@
-# Still have to do the Windows version
+if %1.==. (
+    echo You need a commit message: \"message\".
+) else (
+	lessc static/less/vitality.less static/css/vitality-red.css
+	hugo
+	git add --all .
+	git commit -m "$1"
+	git push -u origin master
+)
